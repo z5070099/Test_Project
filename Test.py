a8fd6b2dce9520e86130e95a324e71b805e0aacd
z5070099 <z5070099@gmail.com>
2024-07-25 11:45:13 +0800

    init

---
+++ Test.py
@@ -0,0 +1,5 @@
+import cv2
+img = cv2.imread('img/Car.jpg')   # 開啟圖片，預設使用 cv2.IMREAD_COLOR 模式
+cv2.imshow('oxxostudio', img)  # 使用名為 oxxostudio 的視窗開啟圖片
+cv2.waitKey(0)                 # 按下任意鍵停止
+cv2.destroyAllWindows() 
