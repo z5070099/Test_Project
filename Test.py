5c76d684689bd89d4b3ccb718511bc601f4ead10
z5070099 <z5070099@gmail.com>
2024-07-25 12:22:10 +0800

    Grayscale

--- Test.py
+++ Test.py
@@ -1,5 +1,3 @@
 import cv2
-img = cv2.imread('img/Car.jpg')   # 開啟圖片，預設使用 cv2.IMREAD_COLOR 模式
-cv2.imshow('圖片', img)  # 使用名為 oxxostudio 的視窗開啟圖片
-cv2.waitKey(0)                 # 按下任意鍵停止
-cv2.destroyAllWindows() 
+img = cv2.imread('img/Car.jpg', cv2.IMREAD_GRAYSCALE)   # 開啟圖片，預設使用 cv2.IMREAD_COLOR 模式
+cv2.imwrite('./img/123.jpg', img, [cv2.IMWRITE_JPEG_QUALITY,80])
