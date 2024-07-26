abb928103f61c680738c43037bb4d4d51c67f4c1
z5070099 <z5070099@gmail.com>
2024-07-26 21:41:41 +0800

    7/26

--- Test.py
+++ Test.py
@@ -3,10 +3,18 @@ import numpy as np
 import Convert
 
 img = cv2.imread('img/strawberry2.jpg')   # 開啟圖片
-HSIImg = Convert.ToHSI(img)
+img = cv2.resize(img, (400,300), interpolation = cv2.INTER_AREA)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ret, thresh = cv2.threshold(gray, 135, 255, cv2.THRESH_BINARY)
+#HSIImg = Convert.ToHSI(img)
+
+#cv2.imshow('HSI', HSIImg)
+
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(img, contours, 1, (0,255,0), 2)
 
 cv2.imshow('RGB', img)
-cv2.imshow('HSI', HSIImg)
+cv2.imshow('draw', thresh)
 
 key = cv2.waitKey(0)
 if key == ord('q'):
